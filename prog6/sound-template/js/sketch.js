f31a6ed7e6217a61d69884f43a389e74b08d7e46
IanWask <123084828+IanWask@users.noreply.github.com>
2023-03-01 20:36:13 -0600

    Add files via upload

---
+++ prog6/sound-template/js/sketch.js
@@ -0,0 +1,58 @@
+let slider;
+
+const synth = new Tone.PluckSynth();
+const reverb = new Tone.JCReverb(0.4);
+synth.connect(reverb);
+const osc = new Tone.OmniOscillator("C#4", "pwm").start();
+
+const ampEnv = new Tone.AmplitudeEnvelope({
+  attack: 0.1,
+  decay: 0.5,
+  sustain: 0.7,
+  release: 0.2
+})
+
+let notes = {
+  'a': 'C4',
+  's': 'D4',
+  'd': 'E4',
+  'f': 'F4',
+  'g': 'G4',
+  'h': 'A4',
+  'j': 'B4',
+  'k': 'C5'
+}
+
+function mousePressed(){
+  Tone.start();
+}
+
+function setup() {
+  createCanvas(400, 400);
+
+  slider = new Nexus.Slider("#slider");
+  reverb.toDestination();
+
+  synth.release = 2;
+  synth.resonance = 0.98;
+
+  slider.on('change', (v) =>  {
+    reverb.roomSize.value = v;
+  }); 
+
+  osc.connect(ampEnv);
+  ampEnv.connect(reverb);
+}
+
+function draw() {
+  background(220);
+}
+
+function keyPressed() {
+  let toPlay = notes[key];
+  console.log(toPlay);
+
+  osc.frequency.value = toPlay;
+  ampEnv.triggerAttackRelease('8n');
+  
+}
